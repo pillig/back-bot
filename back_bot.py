73c5180591803b87b75af68be819436e20bccdc9
Tom Blanchet <TLBlanchet@gmail.com>
2017-04-29 17:06:41 +0000

    No longer buggy - looks at commands (#2)
    We're back in business! (With ~ cmd predicate as well)

--- back_bot.py
+++ back_bot.py
@@ -5,7 +5,7 @@ with open("super_secret_key.txt") as f:
     key = f.readlines()[0].rstrip()
 
 
-back_bot = Bot("!")
+back_bot = Bot("~")
 
 @back_bot.event
 async def on_read():
@@ -14,10 +14,10 @@ async def on_read():
 @back_bot.event
 async def on_message(message):
     #it's a good bug
-        if("back" in message.content.lower()):
+        if(("back" in message.content.lower()) and (message.author.id != back_bot.user.id)):
             print("back found!")
             await back_bot.send_message(message.channel, "Did somebody say back?")
-    
+        await back_bot.process_commands(message)
 #
 @back_bot.command()
 async def im_back(*args):
