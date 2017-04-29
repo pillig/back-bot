639c3b0d4a9f0a284e2fb46581e249456ce7c59f
FrogBomb <TLBlanchet@gmail.com>
2017-04-29 01:28:16 -0600

    Only a fun bug
    k

--- back_bot.py
+++ back_bot.py
@@ -1,17 +1,24 @@
-import discord
+#import discord
 from discord.ext.commands import Bot
 
 with open("super_secret_key.txt") as f:
     key = f.readlines()[0].rstrip()
 
-client = discord.Client()
 
-back_bot = Bot(command_prefix="~")
+back_bot = Bot("!")
 
 @back_bot.event
 async def on_read():
     print("Back into action")
-
+    
+@back_bot.event
+async def on_message(message):
+    #it's a good bug
+        if("back" in message.content.lower()):
+            print("back found!")
+            await back_bot.send_message(message.channel, "Did somebody say back?")
+    
+#
 @back_bot.command()
 async def im_back(*args):
     return await back_bot.say("Hi Back")
